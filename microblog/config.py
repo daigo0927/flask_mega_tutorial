6e48251ef68f5225187c065649faea2daacf4ad5
Daigo HIROOKA <Daigo@Daigo-no-MacBook-Air.local>
2019-08-04 20:22:18 +0900

    complete chapter15: a better application structure

--- microblog/config.py
+++ microblog/config.py
@@ -1,5 +1,8 @@
 import os
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
@@ -14,7 +17,7 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['your-email@example.com']
-    POST_PER_PAGE = 25
+    POSTS_PER_PAGE = 25
     LANGUAGES = ['en', 'es']
 
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
